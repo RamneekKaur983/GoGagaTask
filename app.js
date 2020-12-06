f5dd6b30a1f37c4e50009c9910cf86512ac87d49
unknown <ramneek983@gmail.com>
2020-12-07 02:25:28 +0530

    Port CHanged

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.post('/search' , function(req, res)
         }
     })
 })
-app.listen(3000, function() {
+app.listen(process.env.PORT ||3000, function() {
     console.log("Server started on port 3000");
   });
   
